9e82867d7e09dcfd7c2c2604c105af3cd38e0f64
Corey Ballou <ballouc@gmail.com>
2014-01-04 08:10:19 -0500

    Initial commit of extension.

---
+++ README.md
@@ -0,0 +1,9 @@
+# About #
+
+A Google Chrome extension allowing you to save any and all console logs and AJAX requests to a file for debugging purposes.
+
+## Status ##
+
+Currently in alpha and untested. You need to install manually. Here are instructions to do so:
+
+http://developer.chrome.com/extensions/getstarted.html#unpacked
---
+++ scripts/override.js
@@ -0,0 +1,82 @@
+var ConsoleOverride = {
+
+    /**
+     * Stored console log messages.
+     */
+    message: '',
+
+    /**
+     * Inintialize the saver.
+     */
+    init: function() {
+        this.overrideConsoleLog();
+        this.bindWindowClose();
+
+    },
+
+    /**
+     * Handle storing all console.log data in an array, already in string format.
+     */
+    overrideConsoleLog: function() {
+        var that = this,
+            exists = typeof console != 'undefined',
+            _console = exists ? console.log : null;
+
+        // new console log function
+        console.log = function() {
+            var msg;
+
+            if (
+                (Array.prototype.slice.call(arguments)).length == 1
+                && typeof Array.prototype.slice.call(arguments)[0] == 'string'
+            ) {
+                msg = (Array.prototype.slice.call(arguments)).toString();
+            } else {
+                try {
+                    msg = JSON.stringify(Array.prototype.slice.call(arguments));
+                } catch (e) {
+                    msg = Array.prototype.slice.call(arguments);
+                }
+            }
+
+            that.message.append(msg);
+
+            if (_console) {
+                _console.apply(console, arguments);
+            }
+        }
+    },
+
+    /**
+     * Write to file when the window closes (or url changes, or refresh issued).
+     */
+    bindWindowClose: function() {
+        var self = this;
+
+        window.onbeforeunload = function() {
+            self.savetoFile();
+            return 'Saving console.log history to file: ' + filename;
+        }
+    },
+
+    /**
+     * Handle actual file save.
+     */
+    saveToFile: function() {
+        var filename = window.location.hostname + '-' + window.location.pathname.replace('/', '_'),
+            uriContent = "data:application/octet-stream," + encodeURIComponent(this.message),
+            newWindow;
+
+        // add timestamp to filename
+        filename = '-' + new Date().getTime() + '.log',
+
+        // trigger new window for download
+        newWindow = window.open(uriContent, filename);
+    }
+
+};
+
+// Run our kitten generation script as soon as the document's DOM is ready.
+document.addEventListener('DOMContentLoaded', function () {
+  kittenGenerator.requestKittens();
+});
